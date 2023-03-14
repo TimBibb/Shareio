d5e61a1e270b7783ee2086993c8553029512dcea
Tim Bibbee <timothybibbee@gmail.com>
2023-03-13 22:51:07 -0400

    path fixing

--- server/scripts/BeforeInstallHook.sh
+++ server/scripts/BeforeInstallHook.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 yum update -y
+source ~/.nvm/nvm.sh
 pm2 update
