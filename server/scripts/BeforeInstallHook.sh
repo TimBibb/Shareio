023170a0f2f75a9a6ec854e3c80144f120247416
Tim Bibbee <timothybibbee@gmail.com>
2023-03-13 22:46:21 -0400

    add env variable

--- server/scripts/BeforeInstallHook.sh
+++ server/scripts/BeforeInstallHook.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 yum update -y
-$NVM_DIR/versions/node/v16.19.1/bin/pm2 update
+pm2 update
