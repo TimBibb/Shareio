a3a4cdeae1682982201851ab7dd970976a3b6281
Tim Bibbee <timothybibbee@gmail.com>
2023-03-13 22:57:19 -0400

    debug

--- server/scripts/BeforeInstallHook.sh
+++ server/scripts/BeforeInstallHook.sh
@@ -4,4 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 set -e
 yum update -y
+which nvm
+which pm2
+which node
 pm2 update
