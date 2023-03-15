e7ddadbad9516db7078fd31061d8bc32a8dab07e
Tim Bibbee <timothybibbee@gmail.com>
2023-03-15 00:03:03 -0400

    remove previous files

--- server/scripts/BeforeInstallHook.sh
+++ server/scripts/BeforeInstallHook.sh
@@ -4,3 +4,6 @@ source ~/.bashrc
 export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v16.19.1/bin
 yum update -y
 pm2 update
+
+# Remove existing files in the /usr/share/nginx/html directory
+rm -rf /usr/share/nginx/html/*
