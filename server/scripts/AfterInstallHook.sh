4c588251ddfedbde3433ad9d2edfcfdb5e173cd4
Tim Bibbee <timothybibbee@gmail.com>
2023-03-14 23:16:50 -0400

    fixes for pm2

--- server/scripts/AfterInstallHook.sh
+++ server/scripts/AfterInstallHook.sh
@@ -3,4 +3,6 @@ set -e
 export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v16.19.1/bin
 cd /usr/share/nginx/html
 npm install
-npm run build
+
+# Start the server using PM2 with the ecosystem.config.js file
+pm2 start /home/ec2-user/ecosystem.config.js
