7cf12a0b6c807b1190f40a75b9457c85141d667e
Tim Bibbee <timothybibbee@gmail.com>
2023-03-15 00:42:59 -0400

    add health check passer

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+// Add a route handler for the "/" path
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Health check passed" });
 });
 
 app.listen(PORT, () => {
